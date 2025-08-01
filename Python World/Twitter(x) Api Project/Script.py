cbb740dc976155fb17488e872b524c572c32bfa7
rohitrajvaidya5 <rohit.rajvaidyaa@gmail.com>
2025-08-01 10:36:20 +0530

    New Changes In Twitter Project

--- Python World/Twitter(x) Api Project/Script.py
+++ Python World/Twitter(x) Api Project/Script.py
@@ -1,11 +1,11 @@
 import tweepy
 
 # Authenticate
-client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAAIRs3QEAAAAADy2ef%2FfbQy%2BWK5QJyxFy600PlFQ%3D3N2dmQe3Al8zGGwi9dCCtyKt7cxSh96g28hORMnj4NkVxKKZ3s',
-                       consumer_key='LMEUaIxAjA3bSqXrxUUDbYeDV',
-                       consumer_secret='o8qf02oBlIlHPSeooGCX5FagCRHACRnMkbXmcWb8i4R3AX0XNB',
-                       access_token='1950948712840052736-rAUX1GE1kJumstSbw4HMM2pbVe9qRz',
-                       access_token_secret='G4ntHvtbcE9bP81XXd2tqvcr3BB3JGpZRXrx4UFRyNK6j')
+client = tweepy.Client(bearer_token='YOUR_BEARER_TOKEN',
+                       consumer_key='YOUR_API_KEY',
+                       consumer_secret='YOUR_API_SECRET',
+                       access_token='YOUR_ACCESS_TOKEN',
+                       access_token_secret='YOUR_ACCESS_SECRET')
 
 # Post a tweet
 response = client.create_tweet(text="Hello Twitter from Tweepy!")
