13efa8f08df39cb9711d5de6c874c3e75674aecc
rohitrajvaidya5 <rohit.rajvaidyaa@gmail.com>
2025-08-02 18:08:10 +0530

    Tweeter (x) Automation Project

--- Python World/Twitter(x) Api Project/Script.py
+++ Python World/Twitter(x) Api Project/Script.py
@@ -1,11 +1,12 @@
 import tweepy
+import os
 
 # Authenticate
-client = tweepy.Client(bearer_token='YOUR_BEARER_TOKEN',
-                       consumer_key='YOUR_API_KEY',
-                       consumer_secret='YOUR_API_SECRET',
-                       access_token='YOUR_ACCESS_TOKEN',
-                       access_token_secret='YOUR_ACCESS_SECRET')
+client = tweepy.Client(bearer_token= os.getenv("BEARER_TOKEN"),
+                       consumer_key= os.getenv("CONSUMER_KEY"),
+                       consumer_secret=os.getenv("CONSUMER_SECRET"),
+                       access_token= os.getenv("ACCESS_TOKEN"),
+                       access_token_secret=os.getenv("ACCESS_TOKEN_SECRET"))
 
 # Post a tweet
 response = client.create_tweet(text="Hello Twitter from Tweepy!")
