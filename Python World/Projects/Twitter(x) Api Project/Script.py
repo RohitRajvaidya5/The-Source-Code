06169a2f91c6ad8bb1307bb2e11fd4fe2c49ee07
rohitrajvaidya5 <rohit.rajvaidyaa@gmail.com>
2025-08-03 00:23:47 +0530

    Tweet_With_Text_File

--- Python World/Projects/Twitter(x) Api Project/Script.py
+++ Python World/Projects/Twitter(x) Api Project/Script.py
@@ -8,6 +8,12 @@ client = tweepy.Client(bearer_token= os.getenv("BEARER_TOKEN"),
                        access_token= os.getenv("ACCESS_TOKEN"),
                        access_token_secret=os.getenv("ACCESS_TOKEN_SECRET"))
 
+with open("tweet.txt", "r", encoding="utf-8") as tweet_file:
+    tweet_text = tweet_file.read().strip()
+    print(f"Tweet text ({len(tweet_text)} chars):", repr(tweet_text))
+
+print(tweet_text)
+
 # Post a tweet
-response = client.create_tweet(text="Hello Twitter from Tweepy!")
+response = client.create_tweet(text= tweet_text)
 print("Tweet posted:", response)
