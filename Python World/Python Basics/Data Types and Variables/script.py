4e6d61a6571dc5f02f9dedc33c1ded461416c4c7
rohitrajvaidya5 <rohit.rajvaidyaa@gmail.com>
2025-08-11 21:38:45 +0530

    some basics

---
+++ Python World/Python Basics/Data Types and Variables/script.py
@@ -0,0 +1,12 @@
+a = 123
+
+print(a)
+
+a1 = 9
+
+b = "String"
+
+print(a1 + a)
+
+
+print(type(b))
---
+++ Python World/Python Basics/First_Python_Program/Script.py
@@ -0,0 +1,109 @@
+
+# Printing hello world
+print("Hello World")
+
+# Writing poem in python
+print("""People ask me about the world
+whether I think the world is worth
+saving. I think the world is first
+worth loving.  For it is just a marble
+of the human heart. Turn it and behold
+all its contradictions. Sometimes it is azure sapphire,
+sometimes it’s downright gangrenous. Singular, always,
+this tiny gem of life in a fist of dark,
+the earth and the human heart.
+
+People ask me why
+believe in God?
+Why have faith at all? Why not just do
+good deeds? Faith is the pillow
+on which all of my good deeds sleep—
+And whose true religion is not                     an attempt
+to recover the Beloved
+to their bed?
+
+So when I say Amen,
+when I say,  “The dawn is breaking the bones of your heart like twigs,”
+look at every fall we’ve taken, and remember we are still august.
+Consider the tiniest sparrow:
+its belief in its seeming invincibility.
+Its ringing throat.
+Its eggshell bones.  A body so feeble
+all it has is song.    Well, there may be no
+gorgeous harmony for us at the end of the day
+but predators I pray, won’t you feast on this praise?
+
+What I mean is,
+                         Creation will kill you
+                         it is so beautiful. Look at how brightly
+it unmakes
+all its children.
+
+So when I say humanity is my first love,
+I mean 276 girls studying physics,
+80,000 Palestinians and a black boy.
+
+When I say burial, ashes to ashes and dust to dust
+I mean I also saw life today. It fasted
+from sun up to sundown and broke
+with much joy, and there is no shroud
+white enough to smother the muzzle
+of spring, its fatness, its pink.
+
+So when I say feast,
+I mean quince, parabolic
+pear, or peach
+or perhaps
+my tongue,
+or lower,
+my breasts,
+or lower,
+my
+flesh like a claret ribbon in your mouth
+because it’s all love &           it tastes good
+to me, it tastes
+good to me it
+tastes good to me.                 When the fruit in your hand becomes red meat,
+what do you need to know
+about the sine or the cosine, except
+the apple curves to fit
+the vivid frame of your lips.
+And that’s another amen.
+
+
+
+When I say Let's stay
+marooned in this here chapel
+of a few hours, remember how we
+have all kissed someone in the scratched-out
+light. They became a fountain,
+and our mouths were full
+of pennies after.
+
+When I say hell,
+I mean                          He’ll heal.
+Look around you at this forgiving miracle:
+to burn one day and not burn out.
+
+
+
+When I say alleged heaven, remember
+the crime they have tried to make of our living.
+Remember the body
+has so many things to apologize for
+for which it is not sorry. I am not
+
+sorry
+for bearing witness.
+For loving what I could not
+love, for living in the creases
+of your palms like salt. I am your excess
+tears and I am not sorry. I am
+not sorry for turning this into a lament
+or for using familiar words to comfort you,
+for taking up language as the prosthetic.
+A prayer for a phantom limb so that I can hold this world
+with extra hands. That’s how much I want
+to keep it.
+How much
+I want to keep you safe.""")
